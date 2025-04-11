aaca7ff7329a9e55ba18d8924aaedb2f33171c9d
neilha <neilharrold@gmail.com>
2025-04-11 11:53:12 +0100

    adds organisation helper spec

---
+++ spec/helpers/organisation_helper_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe OrganisationHelper do
+
+  describe "#organisation_type_name" do
+    it "returns a human-readable name given an organisation_type" do
+      expect(organisation_type_name("department")).to eq("Department")
+    end
+
+    it "returns 'other' for an unrecognised organisation_type" do
+      expect(organisation_type_name("something_else")).to eq("Other")
+    end
+  end
+end
